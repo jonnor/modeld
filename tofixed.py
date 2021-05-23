f4829f5e03dae56538ea547520f0ff5d42e312dc
Jon Nordby <jononor@gmail.com>
2021-05-24 00:55:59 +0200

    Script for setting ONNX batch size, and converted TensorRT model
    It is much simpler to convert a model which does not have dynamic
    inputs, and the batch size is always one in practice anyway

---
+++ tofixed.py
@@ -0,0 +1,30 @@
+
+import onnx
+  
+def change_input_dim(model):
+    # Use some symbolic name not used for any other dimension
+    sym_batch_dim = "N"
+    # or an actal value
+    actual_batch_dim = 1
+
+    # The following code changes the first dimension of every input to be batch-dim
+    # Modify as appropriate ... note that this requires all inputs to
+    # have the same batch_dim 
+    inputs = model.graph.input
+    for input in inputs:
+        # Checks omitted.This assumes that all inputs are tensors and have a shape with first dim.
+        # Add checks as needed.
+        dim1 = input.type.tensor_type.shape.dim[0]
+        # update dim to be a symbolic value
+        #dim1.dim_param = sym_batch_dim
+        # or update it to be an actual value:
+        dim1.dim_value = actual_batch_dim
+
+def apply(transform, infile, outfile):
+    model = onnx.load(infile)
+    transform(model)
+    print("Saving model to", outfile)
+    onnx.save(model, outfile)
+
+print("Running")
+apply(change_input_dim, r"models/supercombo.converted.onnx", r"models/supercombo.fixed.onnx")
